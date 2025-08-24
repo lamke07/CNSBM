3e4050b51d23dab59a583ab5f85e21c325cbab4b
Kevin Lam <62064274+lamke07@users.noreply.github.com>
2025-08-24 16:12:20 +0100

    Update version to 1.0.1

--- cnsbm/__init__.py
+++ cnsbm/__init__.py
@@ -5,7 +5,7 @@ A Python package for stochastic block modeling of categorical matrices with supp
 for missing values and copy number variation analysis.
 """
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __author__ = "Kevin Lam"
 __email__ = "kevin.lam@stat.ubc.ca"
 
